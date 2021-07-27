a05fb1a11480abe63ab384fbb5b81e6c4893ebc0
Guy Mac <hello@guymac.eu>
2021-07-27 12:02:28 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "iwgb-org-uk",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
