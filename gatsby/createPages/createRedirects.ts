d724e62f975dc9f33ac91cd581fba41fabc46515
Guy Mac <hello@guymac.eu>
2021-08-27 00:07:14 +0100

    :bug: i18nise redirects

--- gatsby/createPages/createRedirects.ts
+++ gatsby/createPages/createRedirects.ts
@@ -4,9 +4,18 @@ const redirects = {
   '/donate': '/page/donate',
 };
 
-const createRedirects = ({ actions: { createRedirect } }: CreatePagesArgs) => {
+const createRedirects = (
+  { actions: { createRedirect } }: CreatePagesArgs,
+  locales: string[],
+) => {
   (Object.entries(redirects) as [string, string][])
-    .forEach(([fromPath, toPath]) => createRedirect({ fromPath, toPath }));
+    .forEach(([fromPath, toPath]) => locales
+      .forEach(((locale) => {
+        createRedirect({
+          fromPath: `/${locale}${fromPath}`,
+          toPath,
+        });
+      })));
 };
 
 export default createRedirects;
