f8be083f311f643752ed64ab7f2f0c0facb54fb5
Guy Mac <hello@guymac.eu>
2021-12-23 23:02:13 +0000

    :construction_worker: use JS ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,20 +1,19 @@
-import { createElement, ReactHTMLElement } from 'react';
+import { createElement } from 'react';
 import { dom } from '@fortawesome/fontawesome-svg-core';
-import { RenderBodyArgs } from 'gatsby';
 import CleanCss from 'clean-css';
 
-let styleTag: ReactHTMLElement<HTMLStyleElement> | null = null;
+let styleTag = null;
 
 const { styles } = new CleanCss().minify(dom.css());
 
-const getStyleTag = (): ReactHTMLElement<HTMLStyleElement> => createElement('style', {
+const getStyleTag = () => createElement('style', {
   key: 'gatsby-plugin-fontawesome-css',
   type: 'text/css',
   dangerouslySetInnerHTML: { __html: styles },
 });
 
 // eslint-disable-next-line import/prefer-default-export
-export const onRenderBody = ({ setHeadComponents }: RenderBodyArgs) => {
+export const onRenderBody = ({ setHeadComponents }) => {
   if (styleTag !== null) {
     styleTag = getStyleTag();
   }
